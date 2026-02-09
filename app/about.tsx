c5b2b2f5a29bd87da59784d2f06e778602fff0de
Florian-Grevin <grevflor@gmail.com>
2026-02-09 13:23:27 +0100

    Added Input type on about

--- app/about.tsx
+++ app/about.tsx
@@ -1,7 +1,11 @@
 import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
+
 
 export default function Index() {
+    
+  const [name, setName] = useState('');
   return (
     <View
       style={{
@@ -10,8 +14,23 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>This is the about.</Text>
-      <Button title="Go to Index" onPress={()=>router.push("/")}/>
+
+        <Text>This is the about.</Text>
+        <Button title="Go to Index" onPress={()=>router.push("/")}/>
+    
+        <TextInput
+            placeholder="Enter Name"
+            value={name}
+            onChangeText={setName}
+            style={{ 
+                borderWidth: 1,
+                borderColor: "black",
+                padding: 10,
+                margin: 10,
+                width: "80%",
+            }}
+        />
+        <Text style={{ margin: 10, width: "80%" }}>Name: {name}</Text>
     </View>
   );
 }
